203f8bfbebf97d46981e1f0c5da82030618d2971
Vasilenko Alexander <vasilenkosaha@gmail.com>
2023-04-13 11:47:37 +0600

    2lesson

---
+++ work2.py
@@ -0,0 +1,30 @@
+# money=int(input("Введите количество монет: "))
+# i=0
+# count0=0
+# count1=0
+# while(i<money):
+#     copeika=random.randint(0,1)
+#     print(f"({copeika})",end=' ')
+#     i+=1
+#     if(copeika==0):
+#         count0+=1
+#     else: 
+#         count1+=1
+# if (count0<count1):
+#     print(f"\nПроще перевернуть {count0} монеток решка")
+# else:
+#     print(f"\nПроще перевернуть {count1} монеток орел")
+
+
+# x = int(input())
+# y = int(input())
+# for i in range(x):
+#    for j in range(y):
+#        if x == i + j and y == i * j:
+#            print(i, j)
+
+# num=int(input("Число  "))
+# num2=1
+# while num2<num:
+#     print(num2,end=" ")
+#     num2+=num2
