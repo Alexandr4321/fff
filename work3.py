bf8163e243c3b96778c959891e37abeaa6bebccc
Vasilenko Alexander <vasilenkosaha@gmail.com>
2023-04-17 18:23:53 +0600

    3lesson

---
+++ work3.py
@@ -0,0 +1,19 @@
+# num_list = [1, 21, 3, 18, 20, 6, 5, 8, 489]
+# per=int(input())
+# leng=len(num_list)
+# count=0
+# min_n=max(num_list)*9
+# for i in range(leng):
+#     if num_list[i]==per:
+#         count+=1
+#     else :
+#         for i in range(leng):
+#             num=abs(per-num_list[i])
+#             if num<min_n:
+#                min_n=num
+#                res=num_list[i]
+# if count!=0:      
+#     print(f"есть такое значение {count}")
+# else:
+#     print(f"этого значения нет , самое близкое {res}")
+
