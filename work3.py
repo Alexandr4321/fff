7ed25e73a111c564ea735898a37567db7e475292
Vasilenko Alexander <vasilenkosaha@gmail.com>
2023-04-17 18:25:54 +0600

    3lesson

--- work3.py
+++ work3.py
@@ -17,3 +17,13 @@
 # else:
 #     print(f"этого значения нет , самое близкое {res}")
 
+azb= {1:'AEIOULNSTR',
+      	2:'DG',
+      	3:'BCMP',
+      	4:'FHVWY',
+      	5:'K',
+      	8:'JX',
+      	10:'QZ'}
+
+word= input("Слово: ").upper()
+print(sum([k for i in word for k, v in azb.items() if i in v]))
